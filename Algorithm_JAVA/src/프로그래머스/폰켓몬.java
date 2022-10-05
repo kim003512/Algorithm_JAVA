b07d0d32f0e570ab7af930bd2ac67f97282d1686
DESKTOP-QC5MQK7\KMJ <kim003512@naver.com>
2022-10-05 09:31:13 +0900

    [프로그래머스]_해시(폰켓몬)_Lv1

---
+++ Algorithm_JAVA/src/프로그래머스/폰켓몬.java
@@ -0,0 +1,9 @@
+package 프로그래머스;
+import java.util.HashSet;
+import java.util.Arrays;
+
+public class 폰켓몬 {
+    public int solution(int[] nums) {
+        HashSet<Integer> hashSet = new HashSet<Integer>(Arrays.asList(nums));
+    }
+}
