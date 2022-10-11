d3c6672ddf3a78dcdedbc4adcedf00477fb2f2c8
DESKTOP-QC5MQK7\KMJ <kim003512@naver.com>
2022-10-11 15:46:37 +0900

    BJ[2798]_브루트포스(블랙잭)

---
+++ Algorithm_JAVA/src/solved/Lv2/BJ_2798.java
@@ -0,0 +1,42 @@
+package solved.Lv2;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class BJ_2798 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int nowNum = 0;
+        ArrayList<Integer> addNum = new ArrayList<>();
+
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                for(int k=j+1; k<n; k++) {
+                    nowNum = arr[i] + arr[j] + arr[k];
+
+                    if (nowNum <= m) {
+                        addNum.add(nowNum);
+                    }
+                }
+            }
+        }
+        
+        System.out.println(Collections.max(addNum));
+    }
+}
