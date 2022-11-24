e4c35c105a96c33b3ac829dbefe3fb5d09c29988
DESKTOP-QC5MQK7\KMJ <kim003512@naver.com>
2022-11-24 14:35:26 +0900

    백준_단계별_문자열_BJ10809_알파벳

--- Algorithm_JAVA/src/BJ/단계별/문자열/BJ_10809.java
+++ Algorithm_JAVA/src/BJ/단계별/문자열/BJ_10809.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/*
+ * 시간 : 124ms
+ * now -'a'
+ */
 public class BJ_10809 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,6 +21,9 @@ public class BJ_10809 {
         for(int i=0; i<str.length(); i++) {
             char now = str.charAt(i);
 
+            /*
+             * 같은 문자일 경우 처음 등장하는 위치를 기준으로 하기 때문에
+             */
             if(alphabet[now-'a'] == -1) alphabet[now-'a'] = i;
         }
 
