69966d50c175780fc3cc4e3109c23e2a63e0ef19
DESKTOP-QC5MQK7\KMJ <kim003512@naver.com>
2023-02-14 23:23:41 +0900

    프로그래머스_Lv1_기사단원의 무기

--- Algorithm_JAVA/src/프로그래머스/Lv1/기사단원의_무기.java
+++ Algorithm_JAVA/src/프로그래머스/Lv1/기사단원의_무기.java
@@ -2,9 +2,10 @@ package 프로그래머스.Lv1;
 
 public class 기사단원의_무기 {
     public static void main(String[] args) {
-        System.out.println(solution(10, 3, 2));
+        System.out.println(solution4(5, 3, 2));
     }
 
+    //시간 초과
     public static int solution(int number, int limit, int power) {
         int answer = 0;
 
@@ -22,6 +23,53 @@ public class 기사단원의_무기 {
         }
 
         for(int i : list) answer += i;
+
+        return answer;
+    }
+
+    public static int solution2(int number, int limit, int power) {
+        int answer = 0;
+        int[] list = new int[number+1];
+
+        for(int i=1; i<=number; i++) {
+            int cnt = 0;
+            for(int j=1; j*j<=i; j++) {
+                if(j*j == i) cnt++;
+                else if(i % j == 0) cnt += 2;
+            }
+            list[i] = cnt > limit ? power : cnt;
+            answer += list[i];
+        }
+
+        return answer;
+    }
+
+    public static int solution3(int number, int limit, int power) {
+        int answer = 0;
+
+        for(int i=1; i<=number; i++) {
+            int cnt = 0;
+            for(int j=1; j*j<=i; j++) {
+                if(j *j == i) cnt++;
+                else if(i%j == 0) cnt+=2;
+            }
+            answer += (cnt > limit) ? power : cnt;
+        }
+
+        return answer;
+    }
+
+    public static int solution4(int number, int limit, int power) {
+        int answer = 0;
+
+        for(int i=1; i<=number; i++) {
+            int cnt = 0;
+            for(int j=1; j<=Math.sqrt(i); j++) {
+                if(j *j == i) cnt++;
+                else if(i%j == 0) cnt+=2;
+            }
+            answer += (cnt > limit) ? power : cnt;
+        }
 
         return answer;
     }
