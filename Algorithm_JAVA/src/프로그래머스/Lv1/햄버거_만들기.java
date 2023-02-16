3de57aed55f55f0a356a5c79ce27998af48158f1
DESKTOP-QC5MQK7\KMJ <kim003512@naver.com>
2023-02-16 20:13:41 +0900

    프로그래머스_Lv1_햄버거 만들기

---
+++ Algorithm_JAVA/src/프로그래머스/Lv1/햄버거_만들기.java
@@ -0,0 +1,27 @@
+package 프로그래머스.Lv1;
+
+import java.util.Stack;
+
+public class 햄버거_만들기 {
+    public int solution(int[] ingredient) {
+        int answer = 0;
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i : ingredient) {
+            stack.push(i);
+
+            if(stack.size() >= 4) {
+                if(stack.get(stack.size()-4) == 1 
+                && stack.get(stack.size()-3) == 2
+                && stack.get(stack.size()-2) == 3
+                && stack.get(stack.size()-1) == 1) {
+                    answer++;
+                    for(int j=0; j<4; j++) stack.pop();
+                }
+            }
+        };
+
+        return answer;
+    }
+}
