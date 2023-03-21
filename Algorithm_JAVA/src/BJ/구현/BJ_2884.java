f715b63ec285613a7d70453597036f6552edeff7
DESKTOP-QC5MQK7\KMJ <kim003512@naver.com>
2023-03-22 08:35:20 +0900

    백준_구현_브론즈 3_알람 시계

---
+++ Algorithm_JAVA/src/BJ/구현/BJ_2884.java
@@ -0,0 +1,27 @@
+package BJ.구현;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_2884 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int H = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        if(M < 45) {
+            if(H == 0) H = 23;
+            else H = H-1;
+
+            M = 60 + (M - 45);
+        } else {
+            M -= 45;
+        }
+
+        System.out.print(H + " " + M);
+    }
+}
